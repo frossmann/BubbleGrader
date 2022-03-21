d2d4ccb36091f03de11f2da494bca07ce051f112
Francis Rossmann <frossmann@eoas.ubc.ca>
2022-03-20 19:11:44 -0700

    added a function

--- StringUtils.py
+++ StringUtils.py
@@ -1,2 +1,15 @@
+#%%
+import string
+import numpy as np
+
+
 def tprint(string):
     print("    " + string)
+
+
+def get_alpha(num_options):
+    alphabet = list(string.ascii_lowercase)
+    alpha = dict(zip(np.arange(num_options), alphabet[:num_options]))
+    return alpha
+
+# %%
